4e616581a2c3d3064a634028f6dfed2884bb7835
Emran <emranbspt@gmail.com>
2021-08-28 11:06:30 +0600

    basic route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/Welcome/{name}', function($name){
+
+    return view('welcome',['name'=>$name]);
+
+});
+
+Route::view('/welc','welcome', ['name'=>'Tareq Sir']);
+
+Route::redirect('wec', 'Welcome');
